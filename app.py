1b875dc35af78b54e4078ec2b1cc2c98ebb0c192
Paul Harrison <pharrison31415@gmail.com>
2025-09-26 18:14:28 -0600

    add index, activities, first activity, static pages

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, abort, send_from_directory
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template('index.html')
+
+
+@app.route("/activities")
+def activities():
+    return render_template('activities.html')
+
+
+@app.route("/activities/<path:filename>")
+def activity_page(filename):
+    try:
+        return send_from_directory("templates/activities", filename)
+    except Exception:
+        abort(404)
